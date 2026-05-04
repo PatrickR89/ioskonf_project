83979c679afac7e5c03df9c5c80a29258a50a3a7
Patrick Ružman <patrick.ruzman@bozic.io>
2026-05-04 12:38:20 +0200

    - [ Add ] - empty project

---
+++ dAIly drip/dAIly drip/dAIly_dripApp.swift
@@ -0,0 +1,17 @@
+//
+//  dAIly_dripApp.swift
+//  dAIly drip
+//
+//  Created by Patrick Ružman on 04.05.2026..
+//
+
+import SwiftUI
+
+@main
+struct dAIly_dripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dAIly drip/dAIly dripTests/dAIly_dripTests.swift
@@ -0,0 +1,17 @@
+//
+//  dAIly_dripTests.swift
+//  dAIly dripTests
+//
+//  Created by Patrick Ružman on 04.05.2026..
+//
+
+import Testing
+@testable import dAIly_drip
+
+struct dAIly_dripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
